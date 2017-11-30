a89e4cce987c4f90c00cfff0ffd9bb24c5ba6871
Scarletdeathfox <34130594+Scarletdeathfox@users.noreply.github.com>
2017-11-30 15:52:31 +0200

    Add files via upload

---
+++ Liitmine.py
@@ -0,0 +1,13 @@
+
+print("Sisesta a")
+a= int(input())
+print("Sisesta b")
+b= int(input())
+c = a + b
+print(int(c))
+print("Sisesta d")
+d= int(input())
+print("Sisesta p")
+p= int(input())
+k = d + p
+print(int(k))
