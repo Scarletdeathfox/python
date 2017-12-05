0f7254359988333c5abe527b71ac3c509c609fa2
Scarletdeathfox <34130594+Scarletdeathfox@users.noreply.github.com>
2017-12-05 09:08:44 +0200

    Add files via upload

---
+++ if jne.py
@@ -0,0 +1,19 @@
+from random import randint
+print ("A on")
+a = randint(1, 10)
+print (a)
+from random import randint
+print ("B on")
+b = randint(1, 10)
+print (b)
+c = a * b
+print ("Korruta!")
+sinu_valik = int(input())
+if sinu_valik == c:
+    print("tubli")
+else:
+    print("paha paha")
+
+
+    
+
